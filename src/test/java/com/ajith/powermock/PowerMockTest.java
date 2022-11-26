19042531e9252cd1de277a29e8468c326c569d08
ajithkumar.sankar <mailajithkumars@gmail.com>
2022-11-26 21:27:54 +0530

    Unit test static and private method using Power Mockito

--- src/test/java/com/ajith/powermock/PowerMockTest.java
+++ src/test/java/com/ajith/powermock/PowerMockTest.java
@@ -4,15 +4,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
+
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 @RunWith(PowerMockRunner.class)
@@ -32,7 +32,18 @@ public class PowerMockTest {
         when(dependency.retrieveAllStats()).thenReturn(stats);
         PowerMockito.mockStatic(UtilityClass.class);
         when(UtilityClass.staticMethod(6)).thenReturn(70);
-        systemUnderTest.methodCallingAStaticMethod();
+        int result = systemUnderTest.methodCallingAStaticMethod();
+        assertEquals(70, result);
+        UtilityClass.staticMethod(10);
+    }
+
+    @Test
+    public void testPrivateMethod() throws Exception {
+
+        List<Integer> stats = Arrays.asList(1,2,3);
+        when(dependency.retrieveAllStats()).thenReturn(stats);
+        long result = Whitebox.invokeMethod(systemUnderTest, "privateMethodUnderTest");
+        assertEquals(6, result);
     }
 
 }
